fca7a67c1036f5f954666e905537a9a144e1b4fc
Christian <cgulick@student.fitchburgstate.edu>
2022-07-20 22:54:56 -0400

    Edited gem

--- WARM/Assets/Scripts/OreDestroy.cs
+++ WARM/Assets/Scripts/OreDestroy.cs
@@ -6,32 +6,46 @@ public class OreDestroy : MonoBehaviour
 {
     public GameObject oreMaterial;
     public float oreHealth;
+    public int oreAmmount = 5, x;
+    private Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
-        oreHealth = 100f;
+        oreHealth = 50f;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (oreHealth ==0)
+        if (oreHealth == 0)
         {
+            
             Destroy(gameObject);
+        }
+        else if (oreHealth <= 10)
+        {
+            //DropMaterials();
         }
     }
     
      void OnTriggerEnter2D(Collider2D col) {
         if(col.CompareTag("BasicBullet"))
         {
-            oreHealth -= 2;
+            oreHealth -= 10;
             Debug.Log("Hit");
         }
      }
 
     void DropMaterials()
     {
+       direction = Random.insideUnitCircle.normalized; // Random Direction
 
+        while (x != oreAmmount)
+        {
+            Instantiate(oreMaterial, direction, transform.rotation);
+            x++;
+        }
+        
     }
 }
